586fb41ba54969a2ce5d2d844d1dbdbcccef1dd0
metalsimyaci <metalsimyaci@outlook.com>
2021-04-02 00:47:07 +0300

    Add project files.

---
+++ RabbitMqExamples.Console.Consumer/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMqExamples.Console.Consumer
+{
+    class Program
+    {
+        private const string QUEUE_NAME = "BiQueue";
+        private const string RABBITMQ_HOST = "localhost";
+        private const string FANOUT_EXCHANGE_NAME = "Fanout_Exchange";
+        private const string DIRECT_EXCHANGE_NAME = "Direct_Exchange";
+        private const string TOPIC_EXCHANGE_NAME = "Topic_Exchange";
+        private const string HEADER_EXCHANGE_NAME = "Header_Exchange";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                BasicConsumer();
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+
+            }
+            finally
+            {
+                System.Console.Read();
+            }
+        }
+
+        private static void BasicConsumer()
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+           
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(QUEUE_NAME, false, consumer);
+            consumer.Received += (sender, e) =>
+            {
+                System.Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray())); 
+            };
+        }
+
+        private static void SmartQueueConsumer()
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.BasicQos(0, 1, true);
+            //‘BasicQos’ metodunun;
+            //    prefetchSize parametresi : Mesaj boyutunu ifade eder. 0(sıfır) diyerek ilgilenmediğimizi belirtiyoruz.
+            //    prefetchCount parametresi : Dağıtım adetini ifade eder.
+            //    global parametresi : true ise, tüm consumerların aynı anda prefetchCount
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(QUEUE_NAME, false, consumer);
+            consumer.Received += (sender, e) =>
+            {
+                System.Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+        }
+
+        private static void SmartQueueConsumerWithFanoutExchange()
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(FANOUT_EXCHANGE_NAME, ExchangeType.Fanout);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            channel.QueueBind(queueName,FANOUT_EXCHANGE_NAME,routingKey: "");
+            
+            
+            channel.BasicQos(0, 1, true);
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queueName, false, consumer);
+            consumer.Received += (sender, e) =>
+            {
+                System.Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+        }
+
+        private static void SmartQueueConsumerWithDirectExchange(bool isEvenNumber)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(DIRECT_EXCHANGE_NAME,ExchangeType.Direct);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            var routeKey = isEvenNumber ? "EVEN_NUMBERS" : "ODD_NUMBERS";
+            channel.QueueBind(queueName, DIRECT_EXCHANGE_NAME, routingKey: routeKey);
+
+
+            channel.BasicQos(0, 1, true);
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queueName, false, consumer);
+            consumer.Received += (sender, e) =>
+            {
+                System.Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+        }
+        private static void SmartQueueConsumerWithTopicExchange(int numbers)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(TOPIC_EXCHANGE_NAME, ExchangeType.Topic);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            var routeKey = $"SAYI.RAKAM.{(numbers % 2 == 0 ? "EVEN_NUMBERS" : numbers % 3 == 0 ? "MULTIPLES_OF_TREE" : "ODD_NUMBERS")}";
+            channel.QueueBind(queueName, TOPIC_EXCHANGE_NAME, routingKey: routeKey);
+
+
+            channel.BasicQos(0, 1, true);
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queueName, false, consumer);
+            consumer.Received += (sender, e) =>
+            {
+                System.Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+        }
+
+        private static void SmartQueueConsumerWithHeaderExchange(int numbers)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(HEADER_EXCHANGE_NAME, ExchangeType.Headers);
+
+            var queueName = channel.QueueDeclare().QueueName;
+            var headerProperties = new Dictionary<string, object>()
+            {
+                {"x-match","all"/*any*/ },
+                {"Type", (numbers % 2 == 0 ? "EVEN_NUMBERS" : numbers % 3 == 0 ? "MULTIPLES_OF_TREE" : "ODD_NUMBERS")}
+            };
+            channel.QueueBind(queueName, HEADER_EXCHANGE_NAME, routingKey: string.Empty, headerProperties);
+
+
+            channel.BasicQos(0, 1, true);
+
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume(queueName, false, consumer);
+            consumer.Received += (sender, e) =>
+            {
+                System.Console.WriteLine(Encoding.UTF8.GetString(e.Body.ToArray()));
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+        }
+    }
+}
---
+++ RabbitMqExamples.Console/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitMqExamples.Console
+{
+    class Program
+    {
+        private const string QUEUE_NAME = "BiQueue";
+        private const string RABBITMQ_HOST = "localhost";
+        private const string FANOUT_EXCHANGE_NAME = "Fanout_Exchange";
+        private const string DIRECT_EXCHANGE_NAME = "Direct_Exchange";
+        private const string TOPIC_EXCHANGE_NAME = "Topic_Exchange";
+        private const string HEADER_EXCHANGE_NAME = "Header_Exchange";
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                SmartQueuePublisher();
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                System.Console.Read();
+            }
+        }
+
+       
+        private static void BasicPublisher()
+        {
+            ConnectionFactory factory = new ConnectionFactory {HostName = RABBITMQ_HOST};
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.QueueDeclare(QUEUE_NAME, false, false, true);
+
+            var message = "bi test mesajı";
+            byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(exchange: "", routingKey: QUEUE_NAME, body: byteMessage);
+            System.Console.WriteLine("Message published Success!");
+        }
+
+        private static void SmartQueuePublisher()
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.QueueDeclare(QUEUE_NAME, durable:true, false, false);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var message = $" message - {i}";
+                byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;//RabbitMQ'da Fiziksel kayda alınır.
+
+                channel.BasicPublish(exchange: "", routingKey: QUEUE_NAME, body: byteMessage, basicProperties: properties);
+                System.Console.WriteLine("Message published Success!");
+            }
+
+           
+        }
+
+        private static void SmartQueuePublisherWithFanoutExhange()
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.ExchangeDeclare(FANOUT_EXCHANGE_NAME, ExchangeType.Fanout, true, false, null);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var message = $" message - {i}";
+                byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;//RabbitMQ'da Fiziksel kayda alınır.
+
+                channel.BasicPublish(exchange: FANOUT_EXCHANGE_NAME, routingKey: "", body: byteMessage, basicProperties: properties);
+                System.Console.WriteLine("Message published Success!");
+            }
+
+
+        }
+
+        private static void SmartQueuePublisherWithDirectExhange()
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.ExchangeDeclare(DIRECT_EXCHANGE_NAME, ExchangeType.Direct, true, false, null);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var message = $" message - {i}";
+                byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;//RabbitMQ'da Fiziksel kayda alınır.
+
+                var routeKey = i % 2 == 0 ? "EVEN_NUMBERS" : "ODD_NUMBERS";
+                channel.BasicPublish(exchange: DIRECT_EXCHANGE_NAME, routingKey: routeKey, body: byteMessage, basicProperties: properties);
+                System.Console.WriteLine("Message published Success!");
+            }
+        }
+
+        private static void SmartQueuePublisherWithTopicExhange()
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.ExchangeDeclare(TOPIC_EXCHANGE_NAME, ExchangeType.Topic, true, false, null);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var message = $" message - {i}";
+                byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;//RabbitMQ'da Fiziksel kayda alınır.
+
+                var routeKey = $"SAYI.RAKAM.{(i % 2 == 0 ? "EVEN_NUMBERS" : i % 3 == 0 ? "MULTIPLES_OF_TREE":"ODD_NUMBERS")}";
+                channel.BasicPublish(exchange: TOPIC_EXCHANGE_NAME, routingKey: routeKey, body: byteMessage, basicProperties: properties);
+                System.Console.WriteLine("Message published Success!");
+            }
+        }
+
+        private static void SmartQueuePublisherWithHeaderExhange()
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = RABBITMQ_HOST };
+
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            channel.ExchangeDeclare(HEADER_EXCHANGE_NAME, ExchangeType.Headers, true, false, null);
+
+            for (int i = 0; i < 100; i++)
+            {
+                var message = $" message - {i}";
+                byte[] byteMessage = Encoding.UTF8.GetBytes(message);
+
+                IBasicProperties properties = channel.CreateBasicProperties();
+                properties.Persistent = true;//RabbitMQ'da Fiziksel kayda alınır.
+                properties.Headers = new Dictionary<string, object>()
+                {
+                    {"Type", (i % 2 == 0 ? "EVEN_NUMBERS" : i % 3 == 0 ? "MULTIPLES_OF_TREE" : "ODD_NUMBERS")}
+                };
+
+                channel.BasicPublish(exchange: HEADER_EXCHANGE_NAME, routingKey: string.Empty, body: byteMessage, basicProperties: properties);
+                System.Console.WriteLine("Message published Success!");
+            }
+        }
+
+    }
+
+}
